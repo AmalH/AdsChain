4465a9ea0ab320a832c0fb186da23cf6c7a79aef
AmalH <hichri.amal@hotmail.fr>
2018-02-05 21:25:14 +0100

    configuration tab

---
+++ app/src/main/java/amalhichri/androidprojects/com/adschain/activities/ConfigActivity.java
@@ -0,0 +1,16 @@
+package amalhichri.androidprojects.com.adschain.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import amalhichri.androidprojects.com.adschain.R;
+
+public class ConfigActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+}
