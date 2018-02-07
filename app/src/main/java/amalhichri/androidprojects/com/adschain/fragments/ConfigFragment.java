50e9747907300aa4e29cf8116a12ac4ffd025ab5
AmalH <hichri.amal@hotmail.fr>
2018-02-07 02:24:50 +0100

    wallet tab

--- app/src/main/java/amalhichri/androidprojects/com/adschain/fragments/ConfigFragment.java
+++ app/src/main/java/amalhichri/androidprojects/com/adschain/fragments/ConfigFragment.java
@@ -27,7 +27,7 @@ public class ConfigFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_config_fragment, container, false);
-       options.add("a wish list");options.add("select contacts");
+       options.add("a wish list");options.add("contacts I choose");
        spinnerAdapter = new ArrayAdapter<>(getContext(),R.layout.customspinner_view, options);
        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ((Spinner)v.findViewById(R.id.contactsLimitSpinner)).setAdapter(spinnerAdapter);
